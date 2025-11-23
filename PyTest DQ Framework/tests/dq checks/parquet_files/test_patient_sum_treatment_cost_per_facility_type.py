6d0681f5497781a0a675e3d63a41428e341346a2
BohdanPolulikh <32715501+BohdanPolulikh@users.noreply.github.com>
2025-11-24 01:21:16 +0200

    Create test_patient_sum_treatment_cost_per_facility_type.py

---
+++ PyTest DQ Framework/tests/dq checks/parquet_files/test_patient_sum_treatment_cost_per_facility_type.py
@@ -0,0 +1,60 @@
+"""
+Description: DQ tests for test_facility_type_avg_time_spent_per_visit_date Parquet dataset
+Requirement(s): TICKET-1234
+Author(s): Your Name
+"""
+
+import pytest
+
+@pytest.fixture(scope='module')
+def source_data(db_connection):
+    source_query = """
+    SELECT
+    f.facility_type,
+    CONCAT(p.first_name, ' ', p.last_name) AS full_name,
+    SUM(v.treatment_cost) AS sum_treatment_cost
+    FROM
+    visits v
+    JOIN facilities f 
+    ON f.id = v.facility_id
+    JOIN patients p
+    ON p.id = v.patient_id
+    GROUP BY
+    f.facility_type,
+    full_name;"""
+    data = db_connection.get_data_sql(source_query)
+    df = data.sort_values(["facility_type", "full_name"]).reset_index(drop=True)
+    return df
+
+@pytest.fixture(scope='module')
+def target_data(parquet_reader):
+    target_path = "/var/jenkins_home/workspace/parquet_data/patient_sum_treatment_cost_per_facility_type"
+    data = parquet_reader.process(target_path, include_subfolders=True)
+    df = data.sort_values(["facility_type", "full_name"]).reset_index(drop=True)    
+    return df
+
+@pytest.mark.parquet_data
+@pytest.mark.smoke
+@pytest.mark.patient_sum_treatment_cost_per_facility_type
+def test_check_dataset_is_not_empty(target_data, data_quality_library):
+    data_quality_library.check_dataset_is_not_empty(target_data)
+
+@pytest.mark.parquet_data
+@pytest.mark.patient_sum_treatment_cost_per_facility_type
+def test_check_count(source_data, target_data, data_quality_library):
+    data_quality_library.check_count(source_data, target_data)
+
+@pytest.mark.parquet_data
+@pytest.mark.patient_sum_treatment_cost_per_facility_type
+def test_check_data_full_data_set(source_data, target_data, data_quality_library):
+    data_quality_library.check_data_full_data_set(source_data, target_data)
+
+@pytest.mark.parquet_data
+@pytest.mark.patient_sum_treatment_cost_per_facility_type
+def test_check_duplicates(target_data, data_quality_library):
+    data_quality_library.check_duplicates(target_data)
+
+@pytest.mark.parquet_data
+@pytest.mark.patient_sum_treatment_cost_per_facility_type
+def test_check_not_null_values(target_data, data_quality_library):
+    data_quality_library.check_not_null_values(target_data, ['facility_type', 'full_name', 'sum_treatment_cost'])
