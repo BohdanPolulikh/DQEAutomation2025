79d9b45391986f1e9c106ac37ee59b08f3415a82
BohdanPolulikh <32715501+BohdanPolulikh@users.noreply.github.com>
2025-11-24 04:05:33 +0200

    Update data_quality_validation_library.py

--- PyTest DQ Framework/src/data_quality/data_quality_validation_library.py
+++ PyTest DQ Framework/src/data_quality/data_quality_validation_library.py
@@ -1,6 +1,5 @@
 import pandas as pd
 
-
 class DataQualityLibrary:
     """
     A library of static methods for performing data quality checks on pandas DataFrames.
@@ -9,38 +8,33 @@ class DataQualityLibrary:
     the quality of data in DataFrames. Each method performs a specific data quality
     check and uses assertions to ensure that the data meets the expected conditions.
     """
-
     @staticmethod
     def check_duplicates(df, column_names=None):
         if column_names:
-            df.duplicated(column_names)
+            duplicates = df[df.duplicated(column_names)]
         else:
-            df.duplicated(df.columns)
+            duplicates = df[df.duplicated()]
+        assert duplicates.shape[0] == 0, f"DataFrame contains {duplicates.shape[0]} duplicates!"
 
     @staticmethod
     def check_count(df1, df2):
         """Checking that the number of lines matches"""        
-        if df1.count != df2.count:
-            raise AssertionError(f"""Row count mismatch: source={len(df1)}, target={len(df2)}""")
+        assert df1.shape[0] == df2.shape[0], f"Row count mismatch: source={df1.shape[0]}, target={df2.shape[0]}"
 
     @staticmethod
     def check_data_full_data_set(df1, df2):
-        if not df1.equals(df2):
-            raise AssertionError("Source and target DataFrames do not match.")
+        assert df1.equals(df2), "Source and target DataFrames do not match."
 
     @staticmethod
     def check_dataset_is_not_empty(df):
         """Checking that the DataFrame is not empty"""
-        if df.empty:
-            raise AssertionError("DataFrame is empty.")
+        assert not df.empty, f"DataFrame is empty."
 
     @staticmethod
     def check_not_null_values(df, column_names=None):
-        """Checking for null values in columns (or in the entire df)"""
+        """Checking for null values in columns (or in the entire DataFrame)"""
         if column_names:
             null_cols = df[column_names].isnull().any()
-            if null_cols.any():
-                raise AssertionError(f"Null values found in columns: {list(null_cols[null_cols].index)}")
+            assert not null_cols.any(), f"Null values found in columns: {list(null_cols[null_cols].index)}"
         else:
-            if df.isnull().any().any():
-                raise AssertionError("Null values found in DataFrame.")
+            assert not df.isnull().any().any(), "Null values found in DataFrame."
